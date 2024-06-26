f1b9fea8262a3ade230c09c71c4a9e0531618996
Vinícius Cossio de Oliveira <vinicossio@gmail.com>
2024-06-26 11:04:53 -0300

    #models Client model changed to typescript

---
+++ src/Model/Client.ts
@@ -0,0 +1,38 @@
+class Client{
+
+    private clientid: string;
+    private clientname: string;
+    private clientphone: string;
+
+    constructor(body: Client){
+        this.clientid = body.clientid;
+        this.clientname = body.clientname;
+        this.clientphone = body.clientphone;
+    }
+
+    getId(){
+        return this.clientid;
+    }
+
+    getName(){
+        return this.clientname;
+    }
+
+    getPhone(){
+        return this.clientphone;
+    }
+
+    setId(id: string){
+        this.clientid = id;
+    }
+
+    setName(name: string){
+        this.clientname = name;
+    }
+
+    setPhone(phone: string){
+        this.clientphone = phone;
+    }
+}
+
+module.exports = Client;
